520b6f824f978bfc502bce269cc7588954e713bf
Lukas Ocilka <lukas.ocilka@gmail.com>
2014-04-14 09:18:16 +0200

    if/else conditions moved to "context"

--- test/packages_test.rb
+++ test/packages_test.rb
@@ -16,40 +16,49 @@ describe Yast::Packages do
       Yast::Product.stub(:Product).and_return(nil)
     end
 
-    it "returns biosdevname within the list of packages as required by Kernel params" do
-      Yast::SCR.stub(:Read).with(
-        Yast::Path.new(".target.string"),
-        "/proc/cmdline"
-      ).and_return("install=cd:// vga=0x314 biosdevname=1")
-      expect(Yast::Packages.kernelCmdLinePackages.include?("biosdevname")).to be_true
+    context "when biosdevname behavior explicitly defined on the Kenel command line" do
+      it "returns biosdevname within the list of required packages" do
+        Yast::SCR.stub(:Read).with(
+          Yast::Path.new(".target.string"),
+          "/proc/cmdline"
+        ).and_return("install=cd:// vga=0x314 biosdevname=1")
+        expect(Yast::Packages.kernelCmdLinePackages.include?("biosdevname")).to be_true
+      end
+
+      it "does not return biosdevname within the list of required packages" do
+        Yast::SCR.stub(:Read).with(
+          Yast::Path.new(".target.string"),
+          "/proc/cmdline"
+        ).and_return("install=cd:// vga=0x314 biosdevname=0")
+        expect(Yast::Packages.kernelCmdLinePackages.include?("biosdevname")).to be_false
+      end
     end
 
-    it "does not return biosdevname within the list of packages as not required by Kernel params" do
-      Yast::SCR.stub(:Read).with(
-        Yast::Path.new(".target.string"),
-        "/proc/cmdline"
-      ).and_return("install=cd:// vga=0x314 biosdevname=0")
-      expect(Yast::Packages.kernelCmdLinePackages.include?("biosdevname")).to be_false
+    context "when biosdevname behavior not defined on the Kernel command line" do
+      context "and running on a Dell system" do
+        it "returns biosdevname within the list of packages" do
+          Yast::SCR.stub(:Read).with(
+            Yast::Path.new(".target.string"),
+            "/proc/cmdline"
+          ).and_return("install=cd:// vga=0x314")
+          # 0 means `grep` succeeded
+          Yast::SCR.stub(:Execute).and_return(0)
+          expect(Yast::Packages.kernelCmdLinePackages.include?("biosdevname")).to be_true
+        end
+      end
+
+      context "and running on a non-Dell system" do
+        it "does not return biosdevname within the list of packages" do
+          Yast::SCR.stub(:Read).with(
+            Yast::Path.new(".target.string"),
+            "/proc/cmdline"
+          ).and_return("install=cd:// vga=0x314")
+          # 1 means `grep` has not succeeded
+          Yast::SCR.stub(:Execute).and_return(1)
+          expect(Yast::Packages.kernelCmdLinePackages.include?("biosdevname")).to be_false
+        end
+      end
     end
 
-    it "returns biosdevname within the list of packages if biosdevname not specified as Kernel parameter and if running on a Dell system" do
-      Yast::SCR.stub(:Read).with(
-        Yast::Path.new(".target.string"),
-        "/proc/cmdline"
-      ).and_return("install=cd:// vga=0x314")
-      # 0 means `grep` succeeded
-      Yast::SCR.stub(:Execute).and_return(0)
-      expect(Yast::Packages.kernelCmdLinePackages.include?("biosdevname")).to be_true
-    end
-
-    it "does not return biosdevname within the list of packages if biosdevname not specified as Kernel parameter and if not running on a Dell system" do
-      Yast::SCR.stub(:Read).with(
-        Yast::Path.new(".target.string"),
-        "/proc/cmdline"
-      ).and_return("install=cd:// vga=0x314")
-      # 1 means `grep` has not succeeded
-      Yast::SCR.stub(:Execute).and_return(1)
-      expect(Yast::Packages.kernelCmdLinePackages.include?("biosdevname")).to be_false
-    end
   end
 end
