312e1451db6c2fec9cd74464933ba59b76b44008
Ladislav Slezák <lslezak@suse.cz>
2015-01-23 14:58:03 +0100

    solved another shortcut conflict

--- src/modules/SourceDialogs.rb
+++ src/modules/SourceDialogs.rb
@@ -42,7 +42,7 @@ module Yast
       # radio button
       :https             => N_("HTT&PS..."),
       # radio button
-      :samba             => N_("&SMB/CIFS"),
+      :samba             => N_("S&MB/CIFS"),
       # radio button
       :nfs               => N_("NF&S..."),
       # radio button
@@ -1717,7 +1717,7 @@ module Yast
         protocol_box = Builtins.add(
           protocol_box,
           # radio button
-          RadioButton(Id(:samba), Opt(:notify), _("&SMB/CIFS"))
+          RadioButton(Id(:samba), Opt(:notify), _("S&MB/CIFS"))
         )
         protocol_box = Builtins.add(protocol_box, HStretch())
         protocol_box = RadioButtonGroup(
