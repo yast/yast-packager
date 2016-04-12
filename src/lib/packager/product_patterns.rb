ed13583dc3dd8153766ae602364e193171d724e2
Ladislav Slezák <lslezak@suse.cz>
2016-04-12 17:24:36 +0200

    Select the default product patterns (#154)
    select the default product patterns (FATE#320199)
    
    defined by the 'defaultpattern()' provides
    
    - 3.1.95

---
+++ src/lib/packager/product_patterns.rb
@@ -0,0 +1,145 @@
+
+# ------------------------------------------------------------------------------
+# Copyright (c) 2016 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of version 2 of the GNU General Public License as published by the
+# Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# ------------------------------------------------------------------------------
+#
+
+require "yast"
+
+module Yast
+  # Evaluate the default patterns for the currently selected products
+  class ProductPatterns
+    include Yast::Logger
+
+    def initialize
+      Yast.import "Pkg"
+    end
+
+    # Find the default patterns for all selected products.
+    # @note The package management and the products needs to be initialized and
+    #   selected *before* using this method.
+    # @return [Array<String>] pattern names
+    def names
+      @names ||= find
+    end
+
+    # Select the default patterns to installation
+    # @note The package management and the products needs to be initialized and
+    #   selected *before* using this method.
+    # @return [Boolean] true if all patterns were successfuly selected,
+    #   false otherwise
+    def select
+      # use map + all? to try installing *all* patterns, plain all? would stop
+      # at the first failure
+      names.map { |p| Yast::Pkg.ResolvableInstall(p, :pattern) }.all?
+    end
+
+  private
+
+    # Find the default patterns for all selected products.
+    # @return [Array<String>] pattern names
+    def find
+      products = Yast::Pkg.ResolvableProperties("", :product, "")
+      remove_unselected(products)
+      products.map! { |product| product["name"] }
+      log.info "Found selected products: #{products}"
+
+      patterns = products.map { |p| product_patterns(p) }.flatten.uniq
+      log.info "Default patterns for the selected products: #{patterns.inspect}"
+
+      patterns
+    end
+
+    # Find the default patterns for the product.
+    # @param [String] product product name
+    # @return [Array<String>] pattern names
+    def product_patterns(product)
+      product_dependencies = dependencies(product)
+      product_provides = provides(product_dependencies)
+
+      default_patterns(product_provides)
+    end
+
+    # Find dependencies for the product (it's product package).
+    # @param [String] product product name
+    # @return [Array<Hash>] product dependencies, e.g. [{"provides" => "foo"},
+    #   {"requires" => "bar"}, ...]
+    def dependencies(product)
+      product_dependencies = []
+
+      resolvables = Yast::Pkg.ResolvableProperties(product, :product, "")
+      remove_unselected(resolvables)
+
+      resolvables.each do |resolvable|
+        prod_pkg = resolvable["product_package"]
+        next unless prod_pkg
+
+        release_resolvables = Yast::Pkg.ResolvableDependencies(prod_pkg, :package, "")
+        remove_unselected(release_resolvables)
+
+        release_resolvables.each do |release_resolvable|
+          deps = release_resolvable["deps"]
+          product_dependencies.concat(deps) if deps
+        end
+      end
+
+      log.debug "Product #{product} depependencies: #{product_dependencies}"
+
+      product_dependencies
+    end
+    # Remove not selected resolvables from the list
+    # @param [Array<Hash>] resolvables only the Hashes where the key "status"
+    #   maps to :selected value are kept, the rest is removed
+    def remove_unselected(resolvables)
+      resolvables.select! { |p| p["status"] == :selected }
+    end
+
+    # Collect "provides" dependencies from the list.
+    # @param [Array<Hash>] dependencies all dependencies
+    # @return [Array<String>] only the "provides" dependencies
+    # @example
+    #   provides([{"provides" => "foo"}, {"requires" => "bar"}, ...]) => ["foo"]
+    def provides(dependencies)
+      provides = []
+
+      dependencies.each do |dependency|
+        prov = dependency["provides"]
+        provides << prov if prov
+      end
+
+      log.debug "Collected provides dependencies: #{provides.inspect}"
+
+      provides
+    end
+
+    # Collect default pattern names from the provides list.
+    # The default pattern is described by the tag "defaultpattern(foo)"
+    # @param [Array<String>] provides the "provides" dependencies
+    # @return [Array<String>] the default pattern names, empty if no default
+    #   pattern is found
+    def default_patterns(provides)
+      patterns = []
+
+      provides.each do |provide|
+        # is it a defaultpattern() provides?
+        if provide.match(/\Adefaultpattern\((.*)\)\z/)
+          log.info "Found default pattern provide: #{provide}"
+          patterns << Regexp.last_match[1].strip
+        end
+      end
+
+      log.info "Found default patterns: #{patterns.inspect}"
+
+      patterns
+    end
+  end
+end
---
+++ test/product_patterns_test.rb
@@ -0,0 +1,137 @@
+#! /usr/bin/env rspec
+
+require_relative "test_helper"
+require_relative "product_factory"
+
+require "packager/product_patterns"
+
+Yast.import "Pkg"
+
+describe Yast::ProductPatterns do
+  describe "#names" do
+    it "returns empty list when there are no products" do
+      expect(Yast::Pkg).to receive(:ResolvableProperties).with("", :product, "")
+        .and_return([])
+
+      expect(subject.names).to eq([])
+    end
+
+    it "returns empty list when there is no selected product" do
+      expect(Yast::Pkg).to receive(:ResolvableProperties).with("", :product, "")
+        .and_return([ProductFactory.create_product])
+
+      expect(subject.names).to eq([])
+    end
+
+    it "returns empty list when the product release package is not found" do
+      product = ProductFactory.create_product("status"          => :selected,
+                                              "product_package" => nil)
+
+      expect(Yast::Pkg).to receive(:ResolvableProperties).with("", :product, "")
+        .and_return([product])
+      expect(Yast::Pkg).to receive(:ResolvableProperties).with(product["name"], :product, "")
+        .and_return([product])
+
+      expect(subject.names).to eq([])
+    end
+
+    it "returns the default pattern name from the release package" do
+      default_pattern = "def_pattern"
+      product_package_name = "product-release"
+      product_package = { "name" => product_package_name, "status" => :selected,
+         "deps" => [{ "requires" => "foo" }, { "provides" => "bar" },
+                    { "provides" => "defaultpattern(#{default_pattern})" }] }
+      product = ProductFactory.create_product("status"          => :selected,
+                                              "product_package" => product_package_name)
+
+      expect(Yast::Pkg).to receive(:ResolvableProperties).with("", :product, "")
+        .and_return([product])
+      expect(Yast::Pkg).to receive(:ResolvableProperties).with(product["name"], :product, "")
+        .and_return([product])
+      expect(Yast::Pkg).to receive(:ResolvableDependencies).with(product_package_name, :package, "")
+        .and_return([product_package])
+
+      expect(subject.names).to eq([default_pattern])
+    end
+
+    it "returns the default patterns from all products" do
+      default_pattern1 = "def_pattern1"
+      product_package_name1 = "product-release1"
+      product_package1 = { "name" => product_package_name1, "status" => :selected,
+         "deps" => [{ "requires" => "foo" }, { "provides" => "bar" },
+                    { "provides" => "defaultpattern(#{default_pattern1})" }] }
+      product1 = ProductFactory.create_product("status"          => :selected,
+                                               "product_package" => product_package_name1)
+
+      default_pattern2 = "def_pattern2"
+      product_package_name2 = "product-release2"
+      product_package2 = { "name" => product_package_name2, "status" => :selected,
+         "deps" => [{ "requires" => "foo" }, { "provides" => "bar" },
+                    { "provides" => "defaultpattern(#{default_pattern2})" }] }
+      product2 = ProductFactory.create_product("status"          => :selected,
+                                               "product_package" => product_package_name2)
+
+      expect(Yast::Pkg).to receive(:ResolvableProperties).with("", :product, "")
+        .and_return([product1, product2])
+      expect(Yast::Pkg).to receive(:ResolvableProperties).with(product1["name"], :product, "")
+        .and_return([product1])
+      expect(Yast::Pkg).to receive(:ResolvableProperties).with(product2["name"], :product, "")
+        .and_return([product2])
+      expect(Yast::Pkg).to receive(:ResolvableDependencies).with(product_package_name1, :package, "")
+        .and_return([product_package1])
+      expect(Yast::Pkg).to receive(:ResolvableDependencies).with(product_package_name2, :package, "")
+        .and_return([product_package2])
+
+      expect(subject.names.sort).to eq([default_pattern1, default_pattern2].sort)
+    end
+  end
+
+  describe "#select" do
+    context "no default pattern found" do
+      before do
+        expect(subject).to receive(:names).and_return([])
+      end
+
+      it "does not select anything to install" do
+        expect(Yast::Pkg).to_not receive(:ResolvableInstall)
+
+        subject.select
+      end
+
+      it "returns true (success)" do
+        expect(subject.select).to eq(true)
+      end
+    end
+
+    context "default patterns found" do
+      before do
+        expect(subject).to receive(:names).and_return(["pattern1", "pattern2"])
+      end
+
+      it "selects the default patterns to install" do
+        expect(Yast::Pkg).to receive(:ResolvableInstall).with("pattern1", :pattern)
+        expect(Yast::Pkg).to receive(:ResolvableInstall).with("pattern2", :pattern)
+
+        subject.select
+      end
+
+      it "returns true if all default patterns were selected" do
+        expect(Yast::Pkg).to receive(:ResolvableInstall).with("pattern1", :pattern)
+          .and_return(true)
+        expect(Yast::Pkg).to receive(:ResolvableInstall).with("pattern2", :pattern)
+          .and_return(true)
+
+        expect(subject.select).to eq(true)
+      end
+
+      it "returns false if any default pattern could not be selected" do
+        expect(Yast::Pkg).to receive(:ResolvableInstall).with("pattern1", :pattern)
+          .and_return(true)
+        expect(Yast::Pkg).to receive(:ResolvableInstall).with("pattern2", :pattern)
+          .and_return(false)
+
+        expect(subject.select).to eq(false)
+      end
+    end
+  end
+end
