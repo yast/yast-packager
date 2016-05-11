2331525eb7f17acb7645fbf18bf99b2ec9ca3439
Imobach González Sosa <imobachgs@gmail.com>
2016-05-11 14:33:44 +0100

    Disable uneeded local repositories after installation
    * Refactor PkgFinishClient to inherit from Installation::FinishClient
    * PkgFinishClient now lives in lib/packager/pkg_finish.rb.
    * Add a dependency on yast2 >= 3.1.187
    * Bump version and update changes file

---
+++ src/lib/packager/clients/pkg_finish.rb
@@ -0,0 +1,198 @@
+# encoding: utf-8
+
+# ------------------------------------------------------------------------------
+# Copyright (c) 2016 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of version 2 of the GNU General Public License as published by the
+# Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# ------------------------------------------------------------------------------
+
+require "installation/finish_client"
+require "packages/repository"
+
+module Yast
+  class PkgFinishClient < ::Installation::FinishClient
+    include Yast::I18n
+    include Yast::Logger
+
+    # Path to libzypp repositories
+    REPOS_DIR = "/etc/zypp/repos.d"
+    # Path to failed_packages file
+    FAILED_PKGS_PATH = "/var/lib/YaST2/failed_packages"
+    # Command to create a tar.gz to back-up old repositories
+    TAR_CMD = "mkdir -p '%<target>s' && cd '%<target>s' "\
+      "&& /bin/tar -czf '%<archive>s' '%<source>s'"
+    # Format of the timestamp to be used as repositories backup
+    BACKUP_TIMESTAMP_FORMAT = "%Y%m%d-%H%M%S"
+
+    # Constructor
+    def initialize
+      textdomain "packager"
+
+      Yast.import "Pkg"
+      Yast.import "Installation"
+      Yast.import "Mode"
+      Yast.import "Stage"
+      Yast.import "String"
+      Yast.import "FileUtils"
+      Yast.import "Packages"
+      Yast.import "Directory"
+    end
+
+    # @see Implements ::Installation::FinishClient#modes
+    def modes
+      [:installation, :update, :autoinst]
+    end
+
+    # @see Implements ::Installation::FinishClient#title
+    def title
+      _("Saving the software manager configuration...")
+    end
+
+    # @see Implements ::Installation::FinishClient#write
+    def write
+      # Remove (backup) all sources not used during the update
+      # BNC #556469: Backup and remove all the old repositories before any Pkg::SourceSaveAll call
+      backup_all_target_sources if Stage.initial && Mode.update
+
+      # See bnc #384827, #381360
+      if Mode.update
+        log.info("Adding default repositories")
+        WFM.call("inst_extrasources")
+      end
+
+      # If repositories weren't load during installation (for example, in openSUSE
+      # if online repositories were not enabled), resolvables should be loaded now.
+      Pkg.SourceLoad
+      disable_local_repos
+
+      # save all repositories and finish target
+      Pkg.SourceSaveAll
+      Pkg.TargetFinish
+
+      # save repository metadata cache to the installed system
+      # (needs to be done _after_ saving repositories, see bnc#700881)
+      Pkg.SourceCacheCopyTo(Installation.destdir)
+
+      # copy list of failed packages to installed system
+      if File.exist?(FAILED_PKGS_PATH)
+        ::FileUtils.cp(FAILED_PKGS_PATH, File.join(Installation.destdir, FAILED_PKGS_PATH),
+          preserve: true)
+      end
+    end
+
+  private
+
+    # Backup old sources
+    #
+    # During upgrade, old sources are reinitialized
+    # either as enabled or disabled.
+    # The old sources from target should go away.
+    def backup_all_target_sources
+      if !File.exist?(REPOS_DIR)
+        log.error("Directory #{REPOS_DIR} doesn't exist!")
+        return
+      end
+
+      current_repos = SCR.Read(path(".target.dir"), REPOS_DIR)
+
+      if current_repos.nil? || current_repos.empty?
+        log.warn("There are currently no repos in #{REPOS_DIR} conf dir")
+        return
+      else
+        log.info("These repos currently exist on a target: #{current_repos}")
+      end
+
+      # Backup repos.d
+      repos_backup_dir = File.join(Directory.vardir, "repos.d_backup")
+      backup_old_sources(REPOS_DIR, repos_backup_dir).nil?
+
+      # Clean old repositories
+      remove_old_sources(current_repos)
+
+      # Sync sources
+      sync_target_sources
+
+      nil
+    end
+
+    # Disable given repositories if needed
+    #
+    # Given a local repository:
+    #
+    # * if all products it contains are available through another repository,
+    #   then it will be disabled;
+    # * if some product contained is not available through another
+    #   repository, then it will be left untouched.
+    #
+    # @return [Array<Packages::Repository>] List of disabled repositories
+    def disable_local_repos
+      candidates_repos, other_repos = *::Packages::Repository.enabled.partition(&:local?)
+      products = other_repos.map(&:products).flatten.uniq
+      candidates_repos.each_with_object([]) do |repo, disabled|
+        if repo.products.empty?
+          log.info("Repo #{repo.repo_id} (#{repo.name}) does not have products; ignored")
+          next
+        end
+        uncovered = repo.products.reject { |p| products.include?(p) }
+        if uncovered.empty?
+          log.info("Repo #{repo.repo_id} (#{repo.name}) will be disabled because products " \
+            "are present in other repositories")
+          repo.disable!
+          disabled << repo
+        else
+          log.info("Repo #{repo.repo_id} (#{repo.name}) cannot be disabled because these " \
+            "products are not available through other repos: #{uncovered.map(&:name)}")
+        end
+      end
+    end
+
+    # Backup sources
+    #
+    # @param source [String] Path of sources to backup
+    # @param target [String] Directory to store backup
+    # @return [String,nil] Name of the backup archive (locate in the given target directory);
+    #                      nil if the backup failed
+    def backup_old_sources(source, target)
+      archive_name = "repos_#{Time.now.strftime(BACKUP_TIMESTAMP_FORMAT)}.tgz"
+      compress_cmd = format(TAR_CMD,
+        target: String.Quote(target),
+        archive: archive_name,
+        source: String.Quote(source))
+      cmd = SCR.Execute(path(".target.bash_output"), compress_cmd)
+      if !cmd["exit"].zero?
+        log.error("Unable to backup current repos; Command >#{compress_cmd}< returned: #{cmd}")
+        nil
+      else
+        archive_name
+      end
+    end
+
+    # Remove old sources
+    #
+    # @param [Array<String>] List of repositories to remove
+    def remove_old_sources(repos)
+      repos.each do |repo|
+        file = File.join(REPOS_DIR, repo)
+        log.info("Removing target repository #{file}")
+        if !SCR.Execute(path(".target.remove"), file)
+          log.error("Cannot remove #{repo} file")
+        end
+      end
+      log.info("All old repositories were removed from the target")
+    end
+
+    # Reload the target to sync the removed repositories with libzypp
+    # repomanager
+    def sync_target_sources
+      Pkg.TargetFinish
+      Pkg.TargetInitialize(Installation.destdir)
+    end
+  end
+end
---
+++ test/pkg_finish_test.rb
@@ -0,0 +1,200 @@
+#!/usr/bin/env rspec
+
+require_relative "test_helper"
+require "packager/clients/pkg_finish"
+require "packages/repository"
+require "uri"
+require "tmpdir"
+require "fileutils"
+
+describe Yast::PkgFinishClient do
+  Yast.import "Pkg"
+  Yast.import "Installation"
+  Yast.import "WFM"
+
+  FAILED_PKGS_PATH = "/var/lib/YaST2/failed_packages"
+
+  subject(:client) { Yast::PkgFinishClient.new }
+  let(:repositories) { [] }
+
+  before do
+    allow(Yast::WFM).to receive(:Args).and_return(args)
+    allow(::Packages::Repository).to receive(:enabled).and_return(repositories)
+  end
+
+  describe "Info" do
+    let(:args) { ["Info"] }
+
+    it "returns a hash describing the client" do
+      allow(client).to receive(:_).and_return("title")
+      expect(client.run).to eq({
+          "steps" => 1,
+          "title" => "title",
+          "when" => [:installation, :update, :autoinst]})
+    end
+  end
+
+  describe "Write" do
+    let(:args) { ["Write"] }
+    let(:destdir) { "/mnt" }
+    let(:update) { false }
+
+    before do
+      allow(Yast::Installation).to receive(:destdir).and_return(destdir)
+      allow(Yast::Mode).to receive(:update).and_return(update)
+      allow(Yast::Stage).to receive(:initial).and_return(true)
+      allow(Yast::Pkg).to receive(:SourceLoad)
+    end
+
+    it "saves repository information" do
+      expect(Yast::Pkg).to receive(:SourceLoad)
+      expect(Yast::Pkg).to receive(:SourceSaveAll)
+      expect(Yast::Pkg).to receive(:TargetFinish)
+      expect(Yast::Pkg).to receive(:SourceCacheCopyTo).with(destdir)
+      allow(Yast::WFM).to receive(:Execute)
+      expect(client.run).to be_nil
+    end
+
+    it "copies failed_packages list under destination dir" do
+      stub_const("Yast::Pkg", double("pkg").as_null_object)
+      allow(File).to receive(:exist?).and_call_original
+      expect(File).to receive(:exist?).with(FAILED_PKGS_PATH)
+        .and_return(true)
+      expect(FileUtils).to receive(:cp)
+        .with(FAILED_PKGS_PATH, "#{destdir}#{FAILED_PKGS_PATH}", preserve: true)
+      client.run
+    end
+
+    context "given some local repository" do
+      let(:repositories) { [local_repo, remote_repo] }
+
+      let(:local_repo) do
+        Packages::Repository.new(repo_id: 1, name: "SLE-12-SP2-0", enabled: true,
+          url: URI("cd://dev/sr0"), autorefresh: false)
+      end
+
+      let(:remote_repo) do
+        Packages::Repository.new(repo_id: 2, name: "SLE-12-SP2-Pool", enabled: true,
+          url: URI("http://download.suse.com/sle-12-sp2"), autorefresh: true)
+      end
+
+      let(:sles_product) do
+        Packages::Product.new(name: "SLES", version: "12.2",
+          arch: "x86_64", category: "base", status: :available, vendor: "SUSE")
+      end
+
+      let(:sles_ha_product) do
+        Packages::Product.new(name: "SLESHA", version: "12.2",
+          arch: "x86_64", category: "base", status: :available, vendor: "SUSE")
+      end
+
+      before do
+        allow(local_repo).to receive(:products).and_return([sles_product.clone])
+      end
+
+      context "if their products are available through other repos" do
+        before do
+          allow(remote_repo).to receive(:products).and_return([sles_product.clone])
+        end
+
+        it "disables the local repository" do
+          expect(local_repo).to receive(:disable!)
+          client.run
+        end
+      end
+
+      context "if their products are not available through other repos" do
+        before do
+          allow(remote_repo).to receive(:products).and_return([sles_ha_product])
+        end
+
+        it "does not disable the local repository" do
+          expect(local_repo).to_not receive(:disable!)
+          client.run
+        end
+      end
+
+      context "if does not contain any product" do
+        before do
+          allow(local_repo).to receive(:products).and_return([])
+        end
+
+        it "does not disable the local repository" do
+          allow(client.log).to receive(:info).and_call_original
+          expect(local_repo).to_not receive(:disable!)
+          expect(client.log).to receive(:info).with(/ignored/)
+          client.run
+        end
+      end
+    end
+
+    context "during update" do
+      let(:update) { true }
+      let(:tmpdir) do
+        dir = Dir.mktmpdir
+        FileUtils.remove_entry(dir)
+        Pathname(dir)
+      end
+      let(:repos_dir) { tmpdir.join("repos.d") }
+      let(:vardir) { tmpdir.join("var") }
+
+      before do
+        allow(Yast::Directory).to receive(:vardir).and_return(vardir.to_s)
+        allow(Yast::WFM).to receive(:call)
+        stub_const("Yast::PkgFinishClient::REPOS_DIR", repos_dir.to_s)
+        stub_const("Yast::Pkg", double("pkg").as_null_object)
+      end
+
+      context "when repos.d exists and contains files" do
+        before do
+          FileUtils.mkdir_p(repos_dir)                 # Create repos.d
+          FileUtils.touch(repos_dir.join("yast.repo")) # Add a 'repo'
+        end
+
+        it "saves the repositories at /etc/repos.d" do
+          client.run
+
+          # The backup exists
+          file = Pathname.glob(vardir.join("*")).first
+          expect(file.exist?).to eq(true)
+
+          # The old repos are gone
+          expect(Pathname.glob(repos_dir.join("*"))).to be_empty
+        end
+
+        it "logs an error if compression fails" do
+          allow(Yast::SCR).to receive(:Execute).and_call_original
+          expect(Yast::SCR).to receive(:Execute).
+            with(Yast::Path.new(".target.bash_output"), /tar/).
+            and_return("exit" => -1)
+          expect(client.log).to receive(:error)
+            .with(/Unable to backup/)
+          client.run
+        end
+      end
+
+      context "when repos.d does not exist" do
+        it "logs an error" do
+          expect(client.log).to receive(:error).with(/#{repos_dir} doesn't exist/)
+          client.run
+        end
+      end
+
+      context "when repos.d is empty" do
+        before do
+          FileUtils.mkdir_p(repos_dir) # Create repos.d
+        end
+
+        it "logs a warning" do
+          expect(client.log).to receive(:warn).with(/no repos in #{repos_dir}/)
+          client.run
+        end
+      end
+
+      it "calls inst_extrasources client" do
+        expect(Yast::WFM).to receive(:call).with("inst_extrasources")
+        client.run
+      end
+    end
+  end
+end
