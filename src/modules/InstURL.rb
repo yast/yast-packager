f5776409186e260db5ca6db13461bde068bb9e8c
Imobach González Sosa <igonzalezsosa@suse.com>
2016-06-15 11:26:20 +0100

    Refactor installInf2Url method in InstURL module
    * Drop old code.
    * ZyppRepoURL is expected to be set always.

--- src/modules/InstURL.rb
+++ src/modules/InstURL.rb
@@ -9,17 +9,20 @@
 #
 # $Id$
 require "yast"
+require "uri"
 
 module Yast
   class InstURLClass < Module
+    include Yast::Logger
+
     def main
       textdomain "packager"
       Yast.import "Linuxrc"
       Yast.import "URL"
       Yast.import "CheckMedia"
 
-
-      @is_network = true
+      @is_network = nil
+      @installInf2Url = nil
     end
 
     # Hide Password
@@ -30,67 +33,6 @@ module Yast
         "InstURL::HidePassword() is obsoleted, use URL::HidePassword() instead"
       )
       URL.HidePassword(url)
-    end
-
-    # Get device options for CD/DVD
-    # @return [String] device options (devices=/dev/cdrom)
-    def GetDevicesOption
-      options = ""
-      devicelist = Convert.convert(
-        SCR.Read(path(".probe.cdrom")),
-        :from => "any",
-        :to   => "list <map>"
-      )
-      devlist = Builtins.maplist(devicelist) do |d|
-        Ops.get_string(d, "dev_name", "")
-      end
-
-      ready = []
-      Builtins.foreach(devicelist) do |d|
-        dname = Ops.get_string(d, "dev_name", "")
-        if Ops.get_boolean(d, "notready", true) == false && dname != nil &&
-            dname != ""
-          ready = Builtins.add(ready, dname)
-        end
-      end 
-
-
-
-      devlist = deep_copy(ready) if Builtins.size(ready) != 0
-
-      # add the Linuxrc medium to the beginning
-      repo_url = Linuxrc.InstallInf("RepoURL")
-
-      repo_url = "" if repo_url == nil
-
-      if Builtins.regexpmatch(Builtins.tolower(repo_url), "^cd:") ||
-          Builtins.regexpmatch(Builtins.tolower(repo_url), "^dvd:")
-        Builtins.y2milestone(
-          "Found CD/DVD device in Linuxrc RepoURL: %1",
-          repo_url
-        )
-        linuxrc_device = Builtins.regexpsub(repo_url, "device=(.*)$", "\\1")
-        if linuxrc_device != nil && linuxrc_device != ""
-          linuxrc_device = Ops.add("/dev/", linuxrc_device)
-          Builtins.y2milestone("Using Linuxrc device: %1", linuxrc_device)
-
-          # remove the device if it is already in the list
-          devlist = Builtins.filter(devlist) { |d| d != linuxrc_device }
-          # put the linuxrc device at the beginning
-          devlist = Builtins.prepend(devlist, linuxrc_device)
-
-          Builtins.y2milestone("Using CD/DVD device list: %1", devlist)
-        end
-      end
-
-      Builtins.foreach(devlist) do |d|
-        if d != ""
-          options = Ops.add(options, ",") if options != ""
-          options = Ops.add(options, d)
-        end
-      end
-      options = Ops.add("devices=", options) if options != ""
-      options
     end
 
     #
@@ -142,232 +84,32 @@ module Yast
     # Convert install.inf to a URL useable by the package manager
     # @param [String] extra_dir append path to original URL
     # @return [String] new repository URL
-    def installInf2Url(extra_dir)
-      repo_url = ""
-
-      # bnc #406162
-      repo_url_from_inf = false
-      Builtins.foreach(["ZyppRepoURL"]) do |in_inf_key|
-        repo_url = Linuxrc.InstallInf(in_inf_key)
-        if repo_url != nil && repo_url != ""
-          Builtins.y2milestone(
-            "Using %1 directly from install.inf: %2",
-            in_inf_key,
-            URL.HidePassword(repo_url)
-          )
-          repo_url_from_inf = true
-          raise Break
-        end
-      end
-
-      return repo_url if repo_url_from_inf
-
-      # Initial repository hasn't been found in install.inf
-      # Trying to guess it ...
-      Builtins.y2warning("Initial repository not found in install.inf")
-
-      options = ""
-      url_tokens = {}
-
-      instmode = Linuxrc.InstallInf("InstMode") # mode
-      instmode = "cd" if instmode == nil # defaults to "CD"
-
-      if instmode == "cd" || # CD or DVD
-          instmode == "dvd"
-        @is_network = false
-        options = GetDevicesOption()
-      elsif instmode == "hd" # Harddisk
-        @is_network = false
-        partition = Linuxrc.InstallInf("Partition")
-        if partition != nil
-          options = Ops.add(
-            Ops.add("device=/dev/", partition),
-            "&filesystem=auto"
-          )
+    def installInf2Url(_extra_dir)
+      return @installInf2Url unless @installInf2Url.nil?
+      repo_url = Linuxrc.InstallInf("ZyppRepoURL")
+      @installInf2Url =
+        if repo_url.nil? || repo_url.empty?
+          log.warn "No URL specified through ZyppRepoURL."
+          "cd:///"
         else
-          Builtins.y2error("no partition specified")
+          log.info "Using ZyppRepoURL: #{URL.HidePassword(repo_url)}"
+          repo_url
         end
-      end
-
-      Ops.set(url_tokens, "scheme", instmode)
-
-      if @is_network
-        username = Linuxrc.InstallInf("Username")
-        if username != nil && username != ""
-          Ops.set(url_tokens, "user", username)
-          password = Linuxrc.InstallInf("Password")
-          if password != nil && password != ""
-            Ops.set(url_tokens, "pass", password)
-          end
-        end
-        servername = Linuxrc.InstallInf("ServerName")
-        server = Linuxrc.InstallInf("Server")
-        serverip = Linuxrc.InstallInf("ServerIP")
-
-        if servername != nil && servername != ""
-          Ops.set(url_tokens, "host", servername)
-        elsif server != nil && server != ""
-          Ops.set(url_tokens, "host", server)
-        elsif serverip != nil && serverip != ""
-          Ops.set(url_tokens, "host", serverip)
-        end
-      end # is_network
-
-      isoimg = ""
-      serverdir = Linuxrc.InstallInf("Serverdir")
-      if Linuxrc.InstallInf("SourceType") == "file"
-        if serverdir != "" && serverdir != nil
-          sd_items = Builtins.splitstring(serverdir, "/")
-          sd_items = Builtins.filter(sd_items) { |i| i != "" }
-          last = Ops.subtract(Builtins.size(sd_items), 1)
-          isoimg = Ops.get(sd_items, last, "")
-          Ops.set(sd_items, last, "")
-          serverdir = Builtins.mergestring(sd_items, "/")
-        end
-      end
-
-      # 	if (((instmode == "hd") || is_network)				// if serverdir needed
-      # 	    && ((serverdir != nil) && (serverdir != "")))		// and is valid
-      # 	{
-      # 	    // for smb mounts it is usual to not have a leading slash
-      # 	    if (substring (serverdir, 0, 1) != "/")
-      # 		serverdir = "/" + serverdir;
-      # 	}
-      share = Linuxrc.InstallInf("Share")
-
-      if extra_dir != ""
-        if serverdir != nil
-          # avoid too many slashes
-          if Builtins.findlastof(serverdir, "/") ==
-              Ops.subtract(Builtins.size(serverdir), 1)
-            serverdir = Builtins.substring(
-              serverdir,
-              0,
-              Ops.subtract(Builtins.size(serverdir), 1)
-            )
-          end
-
-          slash = ""
-          slash = "/" if Builtins.substring(extra_dir, 0, 1) != "/"
-          serverdir = Ops.add(Ops.add(serverdir, slash), extra_dir)
-          slash = ""
-        else
-          serverdir = extra_dir
-        end
-      end
-
-      if serverdir != nil && serverdir != ""
-        fs = ""
-        if instmode == "ftp"
-          # ftp://foo/%2fbar is %2fbar on foo  (relative)
-          # ftp://foo/bar is bar on foo (absolute)
-          # ftp://foo//bar is /bar on foo (relative)
-          # Note: %2f is added by URL.ycp if the path starts with /
-          if Builtins.substring(serverdir, 0, 3) == "%2f"
-            serverdir = Ops.add("/", Builtins.substring(serverdir, 3))
-          end
-          fs = serverdir
-        elsif instmode == "smb" && share != nil && share != ""
-          fs = Ops.add(Ops.add(share, "/"), serverdir)
-        else
-          fs = serverdir
-        end
-        Ops.set(url_tokens, "path", fs)
-      else
-        # FIXME don't know why it is needed
-        # Needed as a seperator between URL and options (!)
-        # bnc#571648 - smb installation source: linuxrc path failed for YaST repositories
-        if instmode == "smb" && share != nil && share != ""
-          Ops.set(url_tokens, "path", Ops.add(share, "/"))
-        else
-          Ops.set(url_tokens, "path", "/")
-        end
-      end
-
-      port = Linuxrc.InstallInf("Port")
-      Ops.set(url_tokens, "port", port) if port != nil && port != ""
-
-      url = URL.Build(url_tokens)
-      option_separator = "?"
-
-      if @is_network
-        proxy = Linuxrc.InstallInf("Proxy")
-        if proxy != nil && proxy != ""
-          url = Ops.add(
-            Ops.add(Ops.add(url, option_separator), "proxy="),
-            proxy
-          )
-          option_separator = "&"
-        end
-        proxyport = Linuxrc.InstallInf("ProxyPort")
-        if proxyport != nil && proxyport != ""
-          url = Ops.add(
-            Ops.add(Ops.add(url, option_separator), "proxyport="),
-            proxyport
-          )
-          option_separator = "&"
-        end
-        proxyproto = Linuxrc.InstallInf("ProxyProto")
-        if proxyproto != nil && proxyproto != ""
-          url = Ops.add(
-            Ops.add(Ops.add(url, option_separator), "proxyproto="),
-            proxyproto
-          )
-          option_separator = "&"
-        end
-        proxyuser = Linuxrc.InstallInf("ProxyUser")
-        if proxyuser != nil && proxyuser != ""
-          url = Ops.add(
-            Ops.add(Ops.add(url, option_separator), "proxyuser="),
-            proxyuser
-          )
-          option_separator = "&"
-        end
-        proxypassword = Linuxrc.InstallInf("ProxyPassword")
-        if proxypassword != nil && proxypassword != ""
-          url = Ops.add(
-            Ops.add(Ops.add(url, option_separator), "proxypassword="),
-            proxypassword
-          )
-          option_separator = "&"
-        end
-        workgroup = Linuxrc.InstallInf("WorkDomain")
-        if workgroup != nil && workgroup != ""
-          url = Ops.add(
-            Ops.add(Ops.add(url, option_separator), "workgroup="),
-            workgroup
-          )
-          option_separator = "&"
-        end
-
-        if instmode == "https"
-          Builtins.y2milestone("HTTPS instmode detected")
-
-          if !SSLVerificationEnabled()
-            Builtins.y2security(
-              "Disabling certificate check for the installation repository"
-            )
-
-            # libzypp uses ssl_verify=no option
-            url = Ops.add(Ops.add(url, option_separator), "ssl_verify=no")
-            option_separator = "&"
-          end
-        end
-      end # is_network
-
-      if options != ""
-        url = Ops.add(Ops.add(url, option_separator), options)
-        option_separator = "&"
-        Builtins.y2milestone("options %1", options)
-      end
-
-      url = Builtins.sformat("iso:/?iso=%1&url=%2", isoimg, url) if isoimg != ""
-
-      Builtins.y2debug("URL %1", URL.HidePassword(url))
-      url
     end
 
-    publish :variable => :is_network, :type => "boolean"
+    # Schemes considered local for installInf2Url
+    LOCAL_SCHEMES = ["cd", "dvd", "hd"]
+
+    # Determines whether the installation URL is remote or not
+    #
+    # @return [Boolean] true if it's remote; false otherwise.
+    # @see installInf2Url
+    def is_network
+      return @is_network unless @is_network.nil?
+      @is_network = !LOCAL_SCHEMES.include?(URI(installInf2Url("")).scheme)
+    end
+
+    publish :function => :is_network, :type => "boolean ()"
     publish :function => :HidePassword, :type => "string (string)"
     publish :function => :RewriteCDUrl, :type => "string (string)"
     publish :function => :installInf2Url, :type => "string (string)"
---
+++ test/inst_url_test.rb
@@ -0,0 +1,68 @@
+#!/usr/bin/rspec
+require_relative "test_helper"
+
+Yast.import "InstURL"
+Yast.import "Linuxrc"
+
+describe Yast::InstURL do
+  subject(:inst_url)  { Yast::InstURL }
+    
+  describe "#installInf2Url" do
+    let(:zypp_repo_url) { "http://opensuse.org/repo" }
+
+    before do
+      Yast::InstURL.main
+      allow(Yast::Linuxrc).to receive(:InstallInf).with("ZyppRepoURL")
+        .and_return(zypp_repo_url)
+    end
+
+    it "returns ZyppRepoURL as defined in install.inf" do
+      expect(inst_url.installInf2Url("")).to eq(zypp_repo_url)
+    end
+
+    context "when extra_dir is specified" do
+      it "ignores extra_dir" do # bug or feature?
+        expect(inst_url.installInf2Url("extra")).to eq(zypp_repo_url)
+      end
+    end
+  end
+
+  describe "#is_network" do
+    before do
+      inst_url.main
+      expect(inst_url).to receive(:installInf2Url).and_return(url)
+    end
+
+    context "when URL is of type cd://" do
+      let(:url) { "cd:///?device=disk/by-id/ata-1" }
+
+      it "returns false" do
+        expect(inst_url.is_network).to eq(false)
+      end
+    end
+
+    context "when URL is of type dvd://" do
+      let(:url) { "dvd:///?device=disk/by-id/ata-1" }
+
+      it "returns false" do
+        expect(inst_url.is_network).to eq(false)
+      end
+    end
+
+    context "when URL is of type hd://" do
+      let(:url) { "hd:///?device=disk/by-id/ata-1" }
+
+      it "returns false" do
+        expect(inst_url.is_network).to eq(false)
+      end
+    end
+
+    context "when URL is remote" do
+      let(:url) { "http://download.opensuse.org/leap/DVD1" }
+
+      it "returns true" do
+        expect(inst_url.is_network).to eq(true)
+      end
+    end
+  end
+end
