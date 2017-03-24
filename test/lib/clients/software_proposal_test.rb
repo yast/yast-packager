0d06ae8b8739489744ca060b86a205df09a4a59e
Martin Vidner <mvidner@suse.cz>
2017-03-24 14:50:09 +0100

    Test #make_proposal

--- test/lib/clients/software_proposal_test.rb
+++ test/lib/clients/software_proposal_test.rb
@@ -38,4 +38,28 @@ describe Yast::SoftwareProposalClient do
       expect(r["workflow_sequence"]).to be_a Symbol
     end
   end
+
+  describe "#make_proposal" do
+    before do
+      allow(Yast::Packages).to receive(:PackagesProposalChanged).and_return false
+    end
+
+    it "reports solver problems if partitioning unchanged" do
+      expect(subject).to receive(:adjust_locales).and_return true
+      expect(subject).to receive(:partitioning_changed?).and_return false
+      expect(Yast::Packages).to receive(:Proposal).and_return({foo: :bar})
+      expect(Yast::Packages).to receive(:solve_errors).and_return(1)
+
+      expect(subject.make_proposal({})).to include("warning_level" => :blocker)
+    end
+
+    it "reports solver problems if partitioning changed" do
+      expect(subject).to receive(:adjust_locales).and_return false
+      expect(subject).to receive(:partitioning_changed?).and_return true
+      expect(Yast::Packages).to receive(:Summary).and_return({foo: :bar})
+      expect(Yast::Packages).to receive(:solve_errors).and_return(1)
+
+      expect(subject.make_proposal({})).to include("warning_level" => :blocker)
+    end
+  end
 end
