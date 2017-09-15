dfd68c8710f8ac718de62991f0497932984ef96c
Imobach González Sosa <igonzalezsosa@suse.com>
2017-09-15 11:22:15 +0100

    Add a mechanism to cache release notes

---
+++ test/lib/release_notes_test.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env rspec
+
+require_relative "../test_helper"
+require "y2packager/release_notes"
+
+describe Y2Packager::ReleaseNotes do
+  subject(:rn) do
+    described_class.new(
+      product_name: "SLES", content: "", user_lang: user_lang, lang: "en_US",
+      format: format, version: version
+    )
+  end
+
+  let(:user_lang) { "de_DE" }
+  let(:format) { :txt }
+  let(:version) { "15.0" }
+
+  describe "#matches?" do
+    context "when language, format and version match" do
+      it "returns true" do
+        expect(rn.matches?(user_lang, format, version)).to eq(true)
+      end
+    end
+
+    context "when language does not match" do
+      it "returns false" do
+        expect(rn.matches?("cz_CZ", format, version)).to eq(false)
+      end
+    end
+
+    context "when format does not match" do
+      it "returns false" do
+        expect(rn.matches?(user_lang, :rtf, version)).to eq(false)
+      end
+    end
+
+    context "when version does not match" do
+      it "returns false" do
+        expect(rn.matches?(user_lang, format, "12.3")).to eq(false)
+      end
+    end
+  end
+end
