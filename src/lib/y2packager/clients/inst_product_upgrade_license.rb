1844c1b14b7e7c2093b63c8ef452a1dcf4646780
Ladislav Slezák <lslezak@suse.cz>
2018-01-22 16:07:42 +0100

    Reset the packages status set by solver

--- src/lib/y2packager/clients/inst_product_upgrade_license.rb
+++ src/lib/y2packager/clients/inst_product_upgrade_license.rb
@@ -64,6 +64,11 @@ module Y2Packager
         @product = Y2Packager::Product.selected_base
         # restore the initial status, the package update will be turned on later again
         Yast::Pkg.PkgReset
+        changed = Yast::Pkg.ResolvableProperties("", :package, "").select do |p|
+          p["status"] != :available || p["status"] != :installed
+        end
+        changed.each { |p| Yast::Pkg.PkgNeutral(p["name"]) }
+
         @product
       end
     end
