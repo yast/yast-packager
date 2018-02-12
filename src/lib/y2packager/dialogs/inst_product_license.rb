40920324e92d9da1aa1ed71fd5eef4c4c40270bd
Imobach González Sosa <igonzalezsosa@suse.com>
2018-02-12 16:36:50 +0000

    InstProductlicense is now a CWM dialog

--- src/lib/y2packager/dialogs/inst_product_license.rb
+++ src/lib/y2packager/dialogs/inst_product_license.rb
@@ -11,16 +11,17 @@
 # ------------------------------------------------------------------------------
 
 require "yast"
-require "ui/installation_dialog"
-require "cgi/util"
+require "cwm"
+require "cwm/dialog"
+require "y2packager/widgets/product_license_translations"
+require "y2packager/widgets/product_license_confirmation"
+
 Yast.import "Language"
-Yast.import "UI"
-Yast.import "Report"
 
 module Y2Packager
   module Dialogs
     # Dialog which shows the user a license and ask for confirmation
-    class InstProductLicense < ::UI::InstallationDialog
+    class InstProductLicense < CWM::Dialog
       # @return [Y2Packager::Product] Product
       attr_reader :product
 
@@ -30,132 +31,26 @@ module Y2Packager
       def initialize(product)
         super()
         @product = product
-        self.language = Yast::Language.language
-        self.confirmed = product.license_confirmed?
       end
 
-      # Handler for the :language action
+      # Returns the dialog title
       #
-      # This happens when the user changes the license language
-      def language_handler
-        self.language = Yast::UI.QueryWidget(Id(:language), :Value)
-        Yast::UI.ReplaceWidget(Id(:license_replace_point), license_content)
-      end
-
-      # Handler for the :license_confirmation action
-      #
-      # This action happens when the user clicks the confirmation checkbox.
-      def license_confirmation_handler
-        @confirmed = Yast::UI.QueryWidget(Id(:license_confirmation), :Value)
-      end
-
-      # Handler for the :next action
-      #
-      # This action happens when the user clicks the 'Next' button
-      def next_handler
-        if confirmed
-          update_product_confirmation
-          finish_dialog(:next)
-        else
-          Yast::Report.Message(_("You must accept the license to install this product"))
-        end
-      end
-
-      # Handler for the :back action
-      #
-      # This action happens when the user clicks the 'Back' button
-      def back_handler
-        update_product_confirmation
-        finish_dialog(:back)
-      end
-
-    private
-
-      # @return [String] Language code (en_US, es_ES, etc.).
-      attr_accessor :language
-      # @return [Boolean] Determines whether the user confirmed the license
-      attr_accessor :confirmed
-
-      # Dialog content
-      #
-      # @see ::UI::Dialog
-      def dialog_content
-        VBox(
-          Left(language_selection),
-          VSpacing(0.5),
-          ReplacePoint(
-            Id(:license_replace_point),
-            license_content
-          ),
-          confirmation_checkbox
-        )
-      end
-
-      # Dialog title
-      #
-      # @see ::UI::Dialog
-      def dialog_title
+      # @return [String] Dialog's title
+      def title
         format(_("%s License Agreement"), product.label)
       end
 
-      # Return the UI for the language selector
-      def language_selection
-        ComboBox(
-          Id(:language),
-          Opt(:notify, :hstretch),
-          _("&License Language"),
-          Yast::Language.GetLanguageItems(:primary)
-        )
-      end
-
-      # Return the UI for the license content
-      def license_content
-        MinWidth(
-          80,
-          RichText(Id(:license_content), formatted_license_text)
-        )
-      end
-
-      # Regexp to determine whether the text is formatted as richtext
-      RICHTEXT_REGEXP = /<\/.*>/
-
-      # Return the license text
+      # Dialog content
       #
-      # It detects whether license text is richtext or not and format it
-      # accordingly.
-      #
-      # @return [String] Formatted license text
-      def formatted_license_text
-        text = product.license(language)
-        if RICHTEXT_REGEXP =~ text
-          text
-        else
-          "<pre>#{CGI.escapeHTML(text)}</pre>"
-        end
-      end
-
-      # Return the UI for the confirmation checkbox
-      def confirmation_checkbox
+      # @return [Yast::Term] Dialog's content
+      def contents
         VBox(
-          VSpacing(0.5),
-          Left(
-            CheckBox(
-              Id(:license_confirmation),
-              Opt(:notify),
-              # license agreement check box label
-              _("I &Agree to the License Terms."),
-              confirmed
-            )
+          Widgets::ProductLicenseTranslations.new(product, Yast::Language.language),
+          HBox(
+            Left(Widgets::ProductLicenseConfirmation.new(product)),
+            HStretch()
           )
         )
-      end
-
-      # Update the product's license confirmation status
-      #
-      # It will not update the status if it has not changed.
-      def update_product_confirmation
-        return if product.license_confirmed? == confirmed
-        product.license_confirmation = confirmed
       end
     end
   end
--- test/lib/dialogs/inst_product_license_test.rb
+++ test/lib/dialogs/inst_product_license_test.rb
@@ -1,3 +1,16 @@
+#!/usr/bin/env rspec
+# ------------------------------------------------------------------------------
+# Copyright (c) 2018 SUSE LLC, All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of version 2 of the GNU General Public License as published by the
+# Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# ------------------------------------------------------------------------------
+
 require_relative "../../test_helper"
 require "y2packager/dialogs/inst_product_license"
 require "y2packager/product"
@@ -5,152 +18,26 @@ require "y2packager/product"
 describe Y2Packager::Dialogs::InstProductLicense do
   subject(:dialog) { described_class.new(product) }
   let(:product) do
-    instance_double(
-      Y2Packager::Product,
-      label:                   "openSUSE",
-      license:                  content,
-      license_confirmed?:       confirmed?,
-      :license_confirmation= => nil
-    )
+    instance_double(Y2Packager::Product)
   end
 
   let(:language) { "en_US" }
-  let(:confirmed?) { false }
-  let(:content) { "content" }
 
-  describe "#run" do
+  describe "#contents" do
     before do
       allow(Yast::Language).to receive(:language).and_return(language)
-      allow(Yast::Language).to receive(:GetLanguageItems).and_return([])
     end
 
-    context "when user accepts the license" do
-      before do
-        allow(Yast::UI).to receive(:QueryWidget).with(Id(:license_confirmation), :Value)
-          .and_return(true)
-        allow(Yast::UI).to receive(:UserInput).and_return(:license_confirmation, button)
-      end
-
-      context "and clicks :next" do
-        let(:button) { :next }
-
-        it "confirms the license" do
-          expect(product).to receive(:license_confirmation=).with(true)
-          dialog.run
-        end
-
-        it "returns :next" do
-          expect(dialog.run).to eq(:next)
-        end
-      end
-
-      context "and clicks :back" do
-        let(:button) { :back }
-
-        it "confirms the license" do
-          expect(product).to receive(:license_confirmation=).with(true)
-          dialog.run
-        end
-
-        it "returns :back" do
-          expect(dialog.run).to eq(:back)
-        end
-      end
+    it "includes a confirmation checkbox" do
+      expect(Y2Packager::Widgets::ProductLicenseConfirmation).to receive(:new)
+        .with(product)
+      dialog.contents
     end
 
-    context "when user does not accept the license" do
-      context "and clicks :next" do
-        before do
-          allow(Yast::UI).to receive(:UserInput).and_return(:next, :back)
-        end
-
-        it "shows a message" do
-          expect(Yast::Report).to receive(:Message)
-          dialog.run
-        end
-      end
-
-      context "and clicks :back" do
-        before do
-          allow(Yast::UI).to receive(:UserInput).and_return(:back)
-        end
-
-        it "returns :back" do
-          expect(dialog.run).to eq(:back)
-        end
-
-        it "does not confirm the license" do
-          expect(product).to_not receive(:license_confirmation=)
-          dialog.run
-        end
-      end
-    end
-
-    context "when the user set as unconfirmed a previously confirmed license" do
-      let(:confirmed?) { true }
-
-      before do
-        allow(Yast::UI).to receive(:QueryWidget).with(Id(:license_confirmation), :Value)
-          .and_return(false)
-      end
-
-      context "and clicks :next" do
-        before do
-          allow(Yast::UI).to receive(:UserInput).and_return(:license_confirmation, :next, :back)
-        end
-
-        it "confirms the license" do
-          expect(product).to receive(:license_confirmation=).with(false)
-          dialog.run
-        end
-
-        it "shows a message" do
-          expect(Yast::Report).to receive(:Message)
-          dialog.run
-        end
-      end
-
-      context "and clicks :back" do
-        let(:button) { :back }
-
-        before do
-          allow(Yast::UI).to receive(:UserInput).and_return(:license_confirmation, button)
-        end
-
-        it "set as unconfirmed a license" do
-          expect(product).to receive(:license_confirmation=).with(false)
-          dialog.run
-        end
-
-        it "returns :back" do
-          expect(dialog.run).to eq(:back)
-        end
-      end
-    end
-
-    context "license formatting" do
-      before do
-        allow(Yast::UI).to receive(:UserInput).and_return(:license_confirmation, :back)
-      end
-
-      context "when license content is richtext" do
-        let(:content) { "<h1>title</h1>" }
-
-        it "does not modify the text" do
-          expect(subject).to receive(:RichText).with(Id(:license_content), content)
-          dialog.run
-        end
-      end
-
-      context "when license content is not richtext" do
-        let(:content) { "SLE 15 > SLE 12" }
-
-        it "converts to richtext" do
-          expect(subject).to receive(:RichText)
-            .with(Id(:license_content), "<pre>SLE 15 &gt; SLE 12</pre>")
-          dialog.run
-        end
-      end
+    it "includes product translations using the current language as default" do
+      expect(Y2Packager::Widgets::ProductLicenseTranslations).to receive(:new)
+        .with(product, language)
+      dialog.contents
     end
   end
 end
