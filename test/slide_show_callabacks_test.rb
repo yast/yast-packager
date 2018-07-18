55152955158721cf73b36b3c45801ac3ea131d76
Ladislav Slezák <lslezak@suse.cz>
2018-07-18 15:18:22 +0200

    Fixed whitespace

--- test/slide_show_callabacks_test.rb
+++ test/slide_show_callabacks_test.rb
@@ -13,14 +13,14 @@ describe Yast::SlideShowCallbacksClass do
     let(:pkg_description) { "pkg_description" }
     let(:pkg_size) { 138510 }
     let(:deleting) { false }
-    
+
     before do
       allow(Yast::PackageSlideShow).to receive(:SlideDisplayStart)
       allow(subject).to receive(:HandleInput)
       allow(Yast::Installation).to receive(:destdir).and_return("/")
       allow(File).to receive(:exist?).and_return(true)
       allow(Yast::SlideShow).to receive(:SetUserAbort)
-      
+
       subject.instance_variable_set(:@ask_again, true)
       subject.instance_variable_set(:@pkg_inprogress, pkg_name)
     end
@@ -30,7 +30,7 @@ describe Yast::SlideShowCallbacksClass do
         # sizes > 8EiB are returned as negative numbers (data overflow)
         expect(Yast::Pkg).to receive(:TargetAvailable).and_return(-42)
         expect(subject).to_not receive(:YesNoAgainWarning)
-        
+
         subject.DisplayStartInstall(pkg_name, pkg_location, pkg_description, pkg_size, deleting)
       end
 
@@ -38,7 +38,7 @@ describe Yast::SlideShowCallbacksClass do
         # 1MiB free space
         expect(Yast::Pkg).to receive(:TargetAvailable).and_return(1 << 20)
         expect(subject).to_not receive(:YesNoAgainWarning)
-        
+
         subject.DisplayStartInstall(pkg_name, pkg_location, pkg_description, pkg_size, deleting)
       end
 
@@ -46,7 +46,7 @@ describe Yast::SlideShowCallbacksClass do
         # 64KiB free space
         expect(Yast::Pkg).to receive(:TargetAvailable).and_return(1 << 16)
         expect(subject).to receive(:YesNoAgainWarning)
-        
+
         subject.DisplayStartInstall(pkg_name, pkg_location, pkg_description, pkg_size, deleting)
       end
     end
