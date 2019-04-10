e4f1b67f271e57f7cb524196ea0b2b8e269f2d89
Stefan Schubert <schubi@suse.de>
2019-04-10 15:56:30 +0200

    testcases

--- test/packages_test.rb
+++ test/packages_test.rb
@@ -482,17 +482,29 @@ describe "Yast::Packages" do
   describe "#product_update_warning" do
     let(:products) { load_zypp("products_update.yml") }
 
-    before do
-      allow(Y2Packager::ProductUpgrade).to receive(:will_be_obsolated_by).and_return([])
+    context "product will be removed due an obsolete" do
+      before do
+        allow(Y2Packager::ProductUpgrade).to receive(:will_be_obsolated_by).and_return(["new_product"])
+      end
+
+      it "returns empty hash" do
+        expect(Yast::Packages.product_update_warning(products)).to eq({})
+      end
     end
 
-    it "returns a hash with warning when there is an automatically removed product" do
-      expect(Yast::Packages.product_update_warning(products)).to include("warning", "warning_level")
-    end
+    context "product will be removed" do
+      before do
+        allow(Y2Packager::ProductUpgrade).to receive(:will_be_obsolated_by).and_return([])
+      end
 
-    it "returns empty hash when there is no automatically removed product" do
-      products.each { |product| product["transact_by"] = :user }
-      expect(Yast::Packages.product_update_warning(products)).to eq({})
+      it "returns a hash with warning when there is an automatically removed product" do
+        expect(Yast::Packages.product_update_warning(products)).to include("warning", "warning_level")
+      end
+
+      it "returns empty hash when there is no automatically removed product" do
+        products.each { |product| product["transact_by"] = :user }
+        expect(Yast::Packages.product_update_warning(products)).to eq({})
+      end
     end
   end
 
