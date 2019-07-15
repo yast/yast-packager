e5c9a3f7980c37c7ef83df17c3375d4dc3a77659
Ladislav Slezák <lslezak@suse.cz>
2019-07-15 15:56:33 +0200

    Fixed an unit test to work also on non-x86_64 archs

--- test/product_location_test.rb
+++ test/product_location_test.rb
@@ -10,14 +10,24 @@ def find_product(arr, product)
   arr.find { |p| p.details.product == product }
 end
 
-# loading all repositories and evaluating the product dependencies
-# using the solver takes some time, run it only once and cache
-# the result for all tests
-dir = File.join(__dir__, "data/zypp/test_offline_repo")
-repo_url = "dir://#{URI.escape(dir)}"
-scan_result = Y2Packager::ProductLocation.scan(repo_url)
+# URL of the local testing repository
+REPO_URL = "dir://#{URI.escape(File.join(DATA_PATH, "zypp/test_offline_repo"))}".freeze
 
 describe Y2Packager::ProductLocation do
+  let(:scan_result) { Y2Packager::ProductLocation.scan(REPO_URL) }
+
+  before do
+    # the testing repository only contains the x86_64 packages/products
+    # and the solver ignores the packages for incompatible architectures,
+    # that means the test would fail anywhere except on x86_64.
+    #
+    # So we modify the "setarch" call to always pass the "x86_64" parameter.
+    allow_any_instance_of(::Solv::Pool).to receive(:setarch)
+      .and_wrap_original do |method, *_args|
+        method.call("x86_64")
+      end
+  end
+
   describe ".scan" do
     it "finds all product repositories" do
       # there are 3 testing product repositories
