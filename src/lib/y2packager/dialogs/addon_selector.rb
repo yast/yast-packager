499486db06dd60cf8714ae8dbbf465fcfc28892d
Ladislav Slezák <lslezak@suse.cz>
2019-11-08 19:59:32 +0100

    Preselect the installed add-ons (jsc#SLE-7101)
    ...when upgrading from the Full installation medium
    
    - 4.2.32

--- src/lib/y2packager/dialogs/addon_selector.rb
+++ src/lib/y2packager/dialogs/addon_selector.rb
@@ -13,7 +13,10 @@
 require "yast"
 require "erb"
 require "ui/installation_dialog"
+require "y2packager/resolvable"
 
+Yast.import "AddOnProduct"
+Yast.import "Mode"
 Yast.import "Report"
 Yast.import "Stage"
 Yast.import "UI"
@@ -77,7 +80,7 @@ module Y2Packager
         "subdirectories. Select which products you want to install.</p>")
       end
 
-      # Handle changing the current item or chaging the selection
+      # Handle changing the current item or changing the selection
       def addon_repos_handler
         current_item = Yast::UI.QueryWidget(Id(:addon_repos), :CurrentItem)
         current_product = products.find { |p| p.dir == current_item }
@@ -113,7 +116,8 @@ module Y2Packager
       attr_writer :selected_products
 
       def selection_content
-        products.map { |p| Item(Id(p.dir), p.summary || p.name) }
+        defaults = preselected_products
+        products.map { |p| Item(Id(p.dir), p.summary || p.name, defaults.include?(p)) }
       end
 
       # Dialog content
@@ -228,6 +232,38 @@ module Y2Packager
 
         # render the ERB template in the context of this object
         erb.result(binding)
+      end
+
+      # return a list of the preselected products
+      # during upgrade we want to preselect the installed products
+      # @return [Array<Y2Packager::ProductLocation>] the products
+      def preselected_products
+        return [] unless Yast::Mode.update
+
+        installed_addons = Y2Packager::Resolvable.find(
+          kind: :product, status: :installed, type: "add-on"
+        ).map(&:name) + Y2Packager::Resolvable.find(
+          kind: :product, status: :removed, type: "add-on"
+        ).map(&:name)
+
+        selected_addons = Y2Packager::Resolvable.find(
+          kind: :product, status: :selected, type: "add-on"
+        ).map(&:name)
+
+        # handle the product renames, if a renamed product was installed
+        # replace it with the new product so the new product is preselected
+        Yast::AddOnProductClass::DEFAULT_PRODUCT_RENAMES.each do |k, v|
+          next unless installed_addons.include?(k)
+
+          installed_addons.delete(k)
+          installed_addons.concat(v)
+        end
+
+        # installed but not selected yet products (to avoid duplicates)
+        products.select do |p|
+          installed_addons.include?(p.details&.product) &&
+            !selected_addons.include?(p.details&.product)
+        end
       end
     end
   end
