1c7f06e8b519a20464c296f7f9b82ca4d99615fe
Ladislav Slezák <lslezak@suse.cz>
2021-03-18 18:15:32 +0100

    YaST Console: removed "Software Repositories" button (bsc#1183687)
    Added "configure_repositories" command to the command
    line interface for experts

---
+++ src/lib/installation/console/plugins/configure_repositories_command.rb
@@ -0,0 +1,30 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) 2021 SUSE LLC, All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of version 2 of the GNU General Public License as published by the
+# Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# ------------------------------------------------------------------------------
+
+module Installation
+  module Console
+    # define the "configure_repositories" command in the expert console
+    class Commands
+      def configure_repositories
+        run_yast_module("repositories")
+      end
+
+    private
+
+      def configure_repositories_description
+        "Run the repository manager. Be careful when using it,\n" \
+        "you might easily break the installer when wrongly used!"
+      end
+    end
+  end
+end
