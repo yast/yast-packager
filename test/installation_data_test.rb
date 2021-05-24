9ffc4b9d27e32438bdfe98d312c456fe9a5772dd
Ladislav Slezák <lslezak@suse.cz>
2021-05-24 15:17:50 +0200

    Update unit test

--- test/installation_data_test.rb
+++ test/installation_data_test.rb
@@ -5,17 +5,17 @@ describe Y2Packager::InstallationData do
   describe "#register_callback" do
     it "adds the default product callback" do
       expect(::Installation::InstallationInfo.instance)
-        .to receive(:callback?).with("installation").and_return(false)
+        .to receive(:callback?).with("packager").and_return(false)
 
       expect(::Installation::InstallationInfo.instance)
-        .to receive(:add_callback).with("installation")
+        .to receive(:add_callback).with("packager")
 
       subject.register_callback
     end
 
     it "does not add the callback if it is already defined" do
       expect(::Installation::InstallationInfo.instance)
-        .to receive(:callback?).with("installation").and_return(true)
+        .to receive(:callback?).with("packager").and_return(true)
 
       expect(::Installation::InstallationInfo.instance)
         .to_not receive(:add_callback)
